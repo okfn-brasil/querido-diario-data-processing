f7d8fba373f92c67bf1004e5c2f2f96a97a4e8ee
Jefersonalves <ferreira.jefersonn@gmail.com>
2023-11-28 18:39:08 -0300

    Remove tratamentos específicos de associações do módulo territories

--- tasks/utils/territories.py
+++ tasks/utils/territories.py
@@ -1,17 +1,11 @@
-import unicodedata
 from typing import Dict, Iterable
 from ..interfaces import DatabaseInterface
-
-
-# target_state = target_state.strip().lower()
-# target_city = _clear_city_name(target_city)
-# territory_data = territory_to_data.get((target_city, target_state))
 
 
 def get_territory_to_data(database: DatabaseInterface):
     territories = _get_territories_gazettes(database)
     territory_to_data = {
-        (_clear_city_name(t["state_code"].lower(), t["territory_name"])): t
+        ((t["state_code"], t["territory_name"])): t
         for t in territories
     }
     return territory_to_data
@@ -38,12 +32,3 @@ def _format_territories_data(data):
         "territory_name": data[1],
         "state_code": data[2],
     }
-
-
-def _clear_city_name(name: str):
-    clean_name = name.replace("'", "")
-    clean_name = unicodedata.normalize("NFD", clean_name)
-    clean_name = clean_name.encode("ascii", "ignore").decode("utf-8")
-    clean_name = clean_name.lower()
-    clean_name = clean_name.strip()
-    return clean_name
