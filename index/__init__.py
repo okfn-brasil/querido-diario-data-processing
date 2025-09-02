d9152a88dbd432a95d0b4a1ceb973fcd9c1a34ce
Diego Rabatone Oliveira <diraol@diraol.eng.br>
2025-09-02 01:09:27 -0300

    lint: Fix

--- index/__init__.py
+++ index/__init__.py
@@ -1,5 +1,5 @@
 from .interfaces import IndexInterface
-from .opensearch import create_index_interface, OpenSearchInterface
+from .opensearch import OpenSearchInterface, create_index_interface
 
 __all__ = [
     "create_index_interface",
